5a07719145d10353953601a059e15c6264101196
Brijesh Patel <b.patel405@mybvc.ca>
2018-10-18 23:44:43 -0600

    login stateless component defined

--- bow-space/src/login/login.js
+++ bow-space/src/login/login.js
@@ -1,14 +1,44 @@
-import React, { Component } from 'react';
-import ReactDOM from "react-dom";
+import React from 'react';
+        
 
+function LoginHeader() {
+    return (
+        <div className = 'row justify-content-md-center'>
+            <div className='row' id='login-header'>
+                <h2 className='mr-sm-2'>Welcome to Bow Space</h2>
+            </div>
+        </div>
+    );
+}
 
-class Login extends Component {
+function LoginForm() {
+    return (
+        <div className= 'container' id='login-form'>
+            <div className = 'row justify-content-md-center'>
+                <div className = 'form-inline'>
+                    <label for="email" className='mr-sm-2'>Username: </label>
+                    <input type="email" className="form-control mr-sm-2" id="email-input"/>
+                </div>
+            </div>
+            <div className = 'row justify-content-md-center'>
+                <div className = 'form-inline'>
+                    < label className = 'mr-sm-2' > Password: </label>
+                    <input type="password" className="form-control mr-sm-2" id="pwd-input"/>
+                </div>
+            </div>
+            < div className = 'row justify-content-md-center mr-sm-2' >
+                <button type="submit" className="btn btn-primary mr-sm-2">Submit</button>
+            </div>
+        </div>
+    );
+}
+
+class Login extends React.Component {
     render() {
         return (
-            <div className='container'>
-                <div className = 'container col-md-4g'>
-                    <h2>Welcome to Bow Space</h2>
-                </div>
+            <div className = 'container-fluid'>
+                <LoginHeader/>
+                <LoginForm/>
             </div>
         );
     }
