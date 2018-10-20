fbc5c9f247d2eee27948e408b223d304ba26285d
Karamullah Agha <k.agha809@mybvc.ca>
2018-10-20 01:24:24 -0600

    login pages updated

--- bow-space/src/login/login.js
+++ bow-space/src/login/login.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
         
 
 function LoginHeader() {
     return (
+
         <div className = 'row justify-content-md-center'>
             <div className='row' id='login-header'>
-                <h2 className='mr-sm-2'>Welcome to Bow Space</h2>
+              
             </div>
         </div>
     );
@@ -14,58 +16,83 @@ function LoginHeader() {
 class LoginForm extends React.Component {
 
     handleSubmit = () => {
-        console.log("----------[handleSubmit]-------");
-        console.log(this.props);
-        let newCredentials = authenticateUser();
+        let email = this.refs.email.value;
+        let pwd = this.refs.pwd.value;
+        let userInputs = {email, pwd};
+        let newCredentials = authenticateUser(userInputs);
         this.props.OnSubmitLogin(newCredentials);
     }
 
     render () {
+        const message = this.props.loginAttempt.message;
         return (
-            <div className= 'container' id='login-form'>
-                <div className = 'row justify-content-md-center'>
-                    <div className = 'form-inline'>
-                        <label for="email" className='mr-sm-2'>Username: </label>
-                        <input type="email" className="form-control mr-sm-2" id="email-input"/>
-                    </div>
-                </div>
-                <div className = 'row justify-content-md-center'>
-                    <div className = 'form-inline'>
-                        < label className = 'mr-sm-2' > Password: </label>
-                        <input type="password" className="form-control mr-sm-2" id="pwd-input"/>
-                    </div>
-                </div>
-                < div className = 'row justify-content-md-center mr-sm-2' >
-                    <button type="submit" className="btn btn-dark mr-sm-2" id='login-btn' onClick={this.handleSubmit}>Login</button>
+
+   
+            <div className= 'row justify-content-md-center' id='login-form'> 
+  
+                     <div class="card  bg-dark mb-3" id='login-form' style={{width:'21em'}}>
+                     <div className = 'card-header'>  <img id = "logo" src={ require('../images/bowspace logo.png') }/></div> 
+  <div class="card-body">
+
+  <label id = "login-head">WELCOME!</label>
+  <label id = "login-msg">Nice to see you again, please login.</label>
+            <div className = 'row justify-content-md-center'>
+                <div className = 'form-group'>
+                    <label id = 'login-userpass' for="email" className='mr-sm-2'>Username: </label>
+                    <input type="email" className="form-control mr-sm-2"  id="email-input" ref='email'/>
                 </div>
             </div>
+            <div className = 'row justify-content-md-center'>
+                <div className = 'form-group'>
+                    <label id = 'login-userpass' className = 'mr-sm-2'> Password: </label>
+                    <input type="password" className="form-control mr-sm-2"  id="pwd-input" ref='pwd'/>
+                </div>
+            </div>
+            <div className = 'row justify-content-md-center'>
+                <label className = 'mr-sm-2' id='invalid-login'>{message}</label>
+            </div>
+            < div className = 'row justify-content-md-center mr-sm-2' >
+                <button type="submit" className="btn btn-primary mr-sm-2" id='login-btn' onClick={this.handleSubmit}>Login</button>
+            </div>
+            </div>
+  </div>
+
+            </div>
+        
         );
     }
 }
 
-function authenticateUser() {
-    //replace with api call
-    let loginCredentials = { status: 'success', userId : 'b.patel405', loginToken : 'bvc'}
+function authenticateUser(userInputs) {
+    let loginCredentials = { status: '', userId : '', loginToken : ''}
+    if (userInputs.email !== '' && userInputs.pwd !== '') {
+        //replace with api call
+        console.log(userInputs.email);
+        console.log(userInputs.pwd);
+        loginCredentials = { status: 'success', userId : 'b.patel405', loginToken : 'bvc'}
+    }
     return loginCredentials;
 }
 
 
 class Login extends React.Component {
 
-    //---------CDM --------
-    componentDidMount() {
-        console.log("-------------[CDM]---------------");
-        console.log(this.props.loginCredentials);
+    RedirectToUserBoard() {
+        return ( 
+            <Redirect to="/user" />
+        );
     }
 
     render() {
-        console.log("------------[login component]---------------");
-        console.log(this.props);
-        const loginCredentials = this.props.loginCredentials;
+        const loginAttempt = this.props.loginAttempt;
+        const loginStatus = loginAttempt.loginStatus;
         return (
-            <div className = 'container-fluid'>
+            <div id = "login-background">
+            <div className = 'container-fluid' >
+                {loginStatus === true ? this.RedirectToUserBoard() : ''}
                 <LoginHeader/>
-                <LoginForm loginCredentials={loginCredentials} OnSubmitLogin={this.props.OnSubmitLogin}/>
+                <LoginForm loginAttempt={loginAttempt} OnSubmitLogin={this.props.OnSubmitLogin}/>
+            </div>
             </div>
         );
     }
