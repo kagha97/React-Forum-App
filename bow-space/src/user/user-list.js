b4102ee69fe5c30c0f61e11fd24562cfd2b27d53
Karamullah Agha <k.agha809@mybvc.ca>
2018-10-26 11:36:04 -0600

    merge

--- bow-space/src/user/user-list.js
+++ bow-space/src/user/user-list.js
@@ -1,42 +1,45 @@
 import React, {Component} from 'react';
-import { Redirect } from 'react-router-dom'
+        
 
 
-class UserInfo extends React.Component {
+function UserInfo() {
+    var name = "First Last";
+    var fAl = name.charAt(0); 
+   // alert(fAl);
+    return (
 
-    render() {
-        return (
-            <div className="card row align-items-center" style={{width: '23rem', background:'#0077b3'}}>
-                <img id = "profile" className="card-img-top" src= { require('../images/avatar.png')} alt="User Profile"/>
-                <div className="card-body ">
-                    <label id = "firstlastname">Karamullah Agha</label>
-                    <label id = "username">k.agha809</label>
-                    <button href="#" id= "logout" class="btn btn-danger" onClick={this.props.OnLogout}>Logout</button>
-                </div>
-            </div>
-        );
-    }
+        <div className="card row align-items-center" style={{width: '23rem', background:'#0077b3'}}>
+        <img id = "profile" className="card-img-top" src= { require('../images/avatar.png')} alt="User Profile Picture"/>
+        <div className="card-body ">
+            <label id = "firstlastname">Karamullah Agha</label>
+            <label id = "username">k.agha809</label>
+            <a href="#" id= "logout" className="btn btn-danger">Logout</a>   
+        </div>
+         <button type="button" className="btn btn-primary btn-lg btn-block" style= {{borderRadius: '0em'}}>My Board</button>
+      </div>
+    );
 }
 
 class UserList extends React.Component {
+   
+   
     render () {
         //can use same method to get user list from api
-        var g = ['name1', 'name2', 'name3', 'name4', 'name4', 'name1', 'name2', 'name3', 'name4', 'name4'];
-        const listItems = g.map((name) =>
-        <li type = "button"  class="bg-primary mb-3 list-group-item text-center d-inline-block" style={{width: '16rem'}}>{name}</li>
-        );
+       
+    
+    
         return (
-            <div>     
-                <label id='user-list'>User List</label>
-            <div className="card" style={{width: '18rem', background: '#0099ff'}}>
-           
-        </div> 
-        <ul className="list-group list-group-flush align-items-center pre-scrollable" style = {{maxHeight: '25em'}}>
-        {listItems}
-        </ul>
-      </div>
-      
-          
+        <div>     
+             <label id='user-list-label'>User List</label>
+             <div className="card" style={{width: '18rem', background: '#0099ff'}}>       
+            </div> 
+                <ul  id = 'user-list' className="list-group list-group-flush align-items-center " style = {{maxHeight: '25em'}}>
+                    <li type = "button" className="bg-primary mb-3 list-group-item text-center d-inline-block" style={{width: '16rem'}}>Karamullah</li>
+                    <li type = "button" className="bg-primary mb-3 list-group-item text-center d-inline-block" style={{width: '16rem'}}>Brijesh</li>
+                    <li type = "button" className="bg-primary mb-3 list-group-item text-center d-inline-block" style={{width: '16rem'}}>Anderson</li>
+                    <li type = "button" className="bg-primary mb-3 list-group-item text-center d-inline-block" style={{width: '16rem'}}>Kush</li>
+              </ul>
+        </div>
         );
     }
 }
@@ -44,16 +47,28 @@ class UserList extends React.Component {
 
 class UserListPanel extends React.Component {
 
+    //---------CDM --------
+    componentDidMount() {
+        console.log("-------------[CDM]---------------");
+        console.log(this.props.loginCredentials);
+    }
+
     render() {
+        console.log("------------[login component]---------------");
+        
         return (
-            <div className="card  row align-items-center" style={{height: '100vh',width: '23rem', background: '#333333'}}>
-                <UserInfo OnLogout={this.props.OnLogout}/>
-                <button type="button" class="btn btn-primary btn-lg btn-block" style= {{borderRadius: '0em'}}>My Board</button>
+            <div id = "main-panel" className="card  row align-items-center " style={{minHeight: '100vh',width: '23rem', background: '#333333'}}>
+                <UserInfo/>
                 <UserList/>
-               <img className = "mt-auto" src={ require('../images/bowspace logo.png') }  alt='bowspace'/>
-                </div>
+               <img id = "bottom-logo" style = {{objectFit: 'contain'}} className = "mt-auto" src={ require('../images/bowspace logo.png') } />
+             </div>
         );
     }
 }
+
+
+
+
+
 
 export default UserListPanel;
