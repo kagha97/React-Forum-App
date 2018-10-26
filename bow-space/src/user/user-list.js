3690b3efe61e38424c7a884596a6f5e372387208
Brijesh Patel <b.patel405@mybvc.ca>
2018-10-26 12:05:32 -0600

    logout event attach to onclick of logout button

--- bow-space/src/user/user-list.js
+++ bow-space/src/user/user-list.js
@@ -2,20 +2,16 @@ import React, {Component} from 'react';
         
 
 
-function UserInfo() {
-    var name = "First Last";
-    var fAl = name.charAt(0); 
-   // alert(fAl);
+function UserInfo(props) {
     return (
-
         <div className="card row align-items-center" style={{width: '23rem', background:'#0077b3'}}>
-        <img id = "profile" className="card-img-top" src= { require('../images/avatar.png')} alt="User Profile Picture"/>
+        <img id = "profile" className="card-img-top" src= { require('../images/avatar.png')} alt="User Profile"/>
         <div className="card-body ">
             <label id = "firstlastname">Karamullah Agha</label>
             <label id = "username">k.agha809</label>
-            <a href="#" id= "logout" className="btn btn-danger">Logout</a>   
+            <button href="#" id= "logout" class="btn btn-danger" onClick={props.OnLogout}>Logout</button>
         </div>
-         <button type="button" className="btn btn-primary btn-lg btn-block" style= {{borderRadius: '0em'}}>My Board</button>
+        <button type="button" className="btn btn-primary btn-lg btn-block" style= {{borderRadius: '0em'}}>My Board</button>
       </div>
     );
 }
@@ -25,14 +21,10 @@ class UserList extends React.Component {
    
     render () {
         //can use same method to get user list from api
-       
-    
-    
         return (
         <div>     
              <label id='user-list-label'>User List</label>
-             <div className="card" style={{width: '18rem', background: '#0099ff'}}>       
-            </div> 
+             <div className="card" style={{width: '18rem', background: '#0099ff'}}></div> 
                 <ul  id = 'user-list' className="list-group list-group-flush align-items-center " style = {{maxHeight: '25em'}}>
                     <li type = "button" className="bg-primary mb-3 list-group-item text-center d-inline-block" style={{width: '16rem'}}>Karamullah</li>
                     <li type = "button" className="bg-primary mb-3 list-group-item text-center d-inline-block" style={{width: '16rem'}}>Brijesh</li>
@@ -58,7 +50,7 @@ class UserListPanel extends React.Component {
         
         return (
             <div id = "main-panel" className="card  row align-items-center " style={{minHeight: '100vh',width: '23rem', background: '#333333'}}>
-                <UserInfo/>
+                <UserInfo OnLogout={this.props.OnLogout}/>
                 <UserList/>
                <img id = "bottom-logo" style = {{objectFit: 'contain'}} className = "mt-auto" src={ require('../images/bowspace logo.png') } />
              </div>
