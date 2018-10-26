a53e300844290721c6e7d841b5cc023d3a828ab6
Brijesh Patel <b.patel405@mybvc.ca>
2018-10-26 15:48:50 -0600

    GetUserAuth() function defined to fetch user credentials

---
+++ bow-space/src/API/fetch.js
@@ -0,0 +1,13 @@
+
+const GetUserAuth = (loginAttempt) => {
+    const URL = 'http://api.bowspace.ca/rest/auth/login';
+    return (Promise.resolve())
+			.then(() => {
+                const RequestOptions = { method:"POST", cache:'no-cache', mode:'cors', credentials:'omit', redirect:'error', body:JSON.stringify(loginAttempt) };
+				return (fetch(URL, RequestOptions));
+			})
+}
+
+
+
+export { GetUserAuth };
