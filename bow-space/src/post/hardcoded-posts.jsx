a0b808eca7aef5dc891698bc7e87a8276dc5d52c
Anderson Viana <a.resendeviana683@mybvc.ca>
2018-10-26 18:35:09 -0600

    view-post and post were added.

---
+++ bow-space/src/post/hardcoded-posts.jsx
@@ -0,0 +1,22 @@
+const userHardcodedPosts = [ // Hardcoded array of post of an user
+    {
+        aTitle : 'Another nice test',
+        anAuthor : 'John Whatever',
+        aDate : new Date(),
+        aBody : 'Whatever whatever...  Blah blah blah... '
+    },
+    {
+        aTitle : 'Extra test',
+        anAuthor : 'Mary Who',
+        aDate : new Date(),
+        aBody : 'Nice text...'
+    },
+    {
+        aTitle : 'Array test',
+        anAuthor : 'Frank Plank',
+        aDate : new Date(),
+        aBody : 'Interesting...'
+    }
+];
+
+export default userHardcodedPosts;
---
+++ bow-space/src/post/post.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Post extends Component {
+    render() { 
+        const myTitle = this.props.newPost.aTitle;
+        const myAuthor = this.props.newPost.anAuthor;
+        const myDate = this.props.newPost.aDate.toLocaleTimeString();
+        const myBody = this.props.newPost.aBody
+        return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4><strong>{myTitle}</strong></h4> 
+                            </div>
+                            <div className="card-body">
+                                {myBody}
+                            </div>
+                            <div className="card-footer">
+                                <strong>by {myAuthor}</strong> - <span className="text-muted">{myDate}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+        </div>
+        );
+    }
+}
+
+export default Post;
