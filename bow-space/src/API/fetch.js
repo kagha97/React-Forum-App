4379cdd8f904625db4892da820db01ea28cd1475
Brijesh Patel <b.patel405@mybvc.ca>
2018-10-28 19:22:01 -0600

    API to fetch userlist and messeges

--- bow-space/src/API/fetch.js
+++ bow-space/src/API/fetch.js
@@ -13,30 +13,61 @@ const GetUserAuth = (loginAttempt) => {
 			});
 }
 
-function GetUserList(token, id) {
-    console.log('my fetch');
-    var url = 'http://api.bowspace.ca/rest/users?userid=' + id +'&keywords=';
-    const headers = new Headers({
-        'Content-Type': 'application/x-www-form-urlencoded',
-    })
-    const otherPrams = {
-        headers: headers,
-        method: 'GET',
-        mode: 'cors',
-        cache: "no-cache",
-        credentials: 'omit',
-        headers : new Headers({
-            'Content-Type': 'application/x-www-form-urlencoded',
 
-        Authorization: 'Bearer' + token
-        })
-    };
-
-
-   return (fetch(url, otherPrams).then(data=>data.json()))
-
+const GetAllRegisteredUsers = (loginToken) => {
+	const URL = 'http://api.bowspace.ca/rest/users';
+	const headers = new Headers({
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Bearer ' + loginToken
+	});
+	const param = '?userid=0&keywords=';
+	return (Promise.resolve())
+			.then(() => {
+                const RequestOptions = { method:"GET", headers : headers, cache:'no-cache', mode:'cors', credentials:'omit', redirect:'error' };
+				return (fetch(URL + param, RequestOptions));
+			})
+			.then((Response) => {
+				return (Response.json())
+			});
 }
 
+const GetMyPost = (loginUserId, loginToken) => {
+	const URL = 'http://api.bowspace.ca/rest/posts';
+	const headers = new Headers({
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Bearer ' + loginToken
+	})
+	const param = '?postid=0&senderid=0&recipientid='+ loginUserId +'&keywords=' 
+	return (Promise.resolve())
+			.then(() => {
+                const RequestOptions = { method:"GET", headers : headers, cache:'no-cache', mode:'cors', credentials:'omit', redirect:'error' };
+				return (fetch(URL + param, RequestOptions));
+			})
+			.then((Response) => {
+				return (Response.json())
+			});
+}
 
+const SendPostMessege = (loginCredentials, messege) => {
+	const post =  {
+		PostId : 0,
+		SenderId : loginCredentials.UserId,
+		RecipientId : messege.ReceipientId,
+		PostHtml: messege.PostHtml
+	}; 
+	const headers = new Headers({
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Bearer ' + loginCredentials.LoginToken
+	})
+    const URL = 'http://api.bowspace.ca/rest/post';
+    return (Promise.resolve())
+			.then(() => {
+                const RequestOptions = { method:"POST", headers : headers, cache:'no-cache', mode:'cors', credentials:'omit', redirect:'error', body:JSON.stringify(post) };
+				return (fetch(URL, RequestOptions));
+			})
+			.then((Response) => {
+				return (Response.json())
+			});
+}
 
-export { GetUserAuth, GetUserList };
+export { GetUserAuth, GetAllRegisteredUsers, GetMyPost, SendPostMessege };
